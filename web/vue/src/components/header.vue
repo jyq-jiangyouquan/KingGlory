db9490dedf973f1282de95cd0f1f9d220e5a9aa2
Jiang You Quan <jiang@qq.com>
2019-08-01 19:54:42 +0800

    背景图片及导航条

--- web/vue/src/components/header.vue
+++ web/vue/src/components/header.vue
@@ -1,56 +1,82 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="header">
+       <div>
+           <div class="mytop"></div>
+           <div>
+               <img src="//ossweb-img.qq.com/upload/webplat/info/yxzj/20190717/830511120753857.jpg" />
+           </div>
+           <div class="my-navigation">
+               <div class="myleft"></div>
+               <div>
+                  <a href="">
+                     <h2>官网首页</h2>HOME    
+                  </a> 
+               </div>
+               <div>
+                   <a href="">
+                     <h2>游戏资料</h2>DATA    
+                  </a>
+               </div>
+               <div>
+                   <a href="">
+                     <h2>内容中心页</h2>CONTENTS    
+                  </a>
+               </div>
+               <div>
+                   <a href="">
+                     <h2>赛事中心</h2>MATCH   
+                  </a>
+               </div>
+               <div>
+                   <a href="">
+                     <h2>百态王者</h2>CULTURE    
+                  </a>
+               </div>
+               <div>
+                   <a href="">
+                     <h2>社区互动</h2>COMMUNTY    
+                  </a>
+               </div>
+               <div>
+                   <a href="">
+                     <h2>玩家支持</h2>PLAYER   
+                  </a>
+               </div> 
+               <div>
+                   <a href="">
+                     <h2>欢迎登录</h2>HOME    
+                   </a>
+               </div>
+               <div class="myleft"></div>
+           </div>
+       </div>    
+    </div>
 </template>
-
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+    
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+h2{margin: 0;}
+  .header>div>div img{
+      width: 100%;
+      float: left;
+  }
+  .mytop{
+      height: 60px;
+  }
+  .my-navigation{
+      width: 100%;
+      height: 105px;
+      background-color:rgba(0, 0, 0, 0.85);
+      position: relative;
+  }
+   .my-navigation>div{
+      padding: 15px;
+      display:inline-flex;
+  }
+  .my-navigation .myleft{
+      width: 10%;
+  }
 </style>
--- web/vue/src/router.js
+++ web/vue/src/router.js
@@ -3,16 +3,16 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // 自定义组件
 import HelloContainer from "./components/HelloWorld.vue"
-// 1：引入Exam01.vue组件
-
+// 1：引入header.vue组件
+import Header from "./components/Header.vue"
 
 Vue.use(Router)
-// 2：为Exam01.vue 配置路径
+// 2：为***.vue 配置路径
 export default new Router({
   routes: [
-    {path:'/',component:HelloContainer}
-    //2.为Exam0..配置路径
-
+    {path:'/',component:HelloContainer},
+    //2.为header..配置路径
+    {path:'/Header',component:Header}
   
   ]
 })
